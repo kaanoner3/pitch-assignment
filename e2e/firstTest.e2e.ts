257f91f15163ca970d95e156e6e7666c09612662
kaanoner3 <oner.kaan.k@gmail.com>
2022-09-02 14:28:18 +0300

    chore: detox setup

---
+++ e2e/firstTest.e2e.ts
@@ -0,0 +1,22 @@
+import {device, element, by, expect} from 'detox';
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp({permissions: {contacts: 'YES'}});
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('sectionList'))).toBeVisible();
+  });
+  it('flatlist should visible when type to search input', async () => {
+    await element(by.id('search-input')).tap();
+    await element(by.id('search-input')).typeText('Anna');
+    await expect(element(by.id('cancel-button'))).toBeVisible(100);
+    await expect(element(by.id('flatlist'))).toBeVisible(50);
+    await expect(element(by.id('sectionList'))).not.toBeVisible();
+  });
+});
--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ export default {
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
   },
+  //testRegex: ['*.test.ts$'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|recoil)',
   ],
